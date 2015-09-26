9bff8172b727818fd6eab40e5a2e7e0dd89b9389
Junwei Pan <jwpan@admb1044.yse.corp.yahoo.com>
2015-09-26 04:16:34 +0000

    Update

--- HDML/tree_based_model/regression_tree.py
+++ HDML/tree_based_model/regression_tree.py
@@ -4,16 +4,18 @@ import xml.dom.minidom
 import sys
 
 class regression_tree:
-    def __init__(self, lst_sample, n_node):
+    def __init__(self, lst_sample, lst_feature, n_node):
+        self.lst_feature = lst_feature
         self.lst_sample = lst_sample
         self.n_node = n_node
         self.lst_leaf = []
+        self.root = None
 
     def fit(self):
         current_n_node = 0
-        root = tree_node(self.lst_sample)
-        mse_min, split_fea, split_val, left_child, right_child = root.try_split_node(criteria = 'MSE')
-        root.split_node(split_fea, split_val)
+        root = tree_node(self.lst_sample, self.lst_feature)
+        mse_min, split_fea_index, split_val, left_child, right_child = root.try_split_node(criteria = 'MSE')
+        root.split_node(split_fea_index, split_val)
         if root.left_child.n_sample > 1:
             self.lst_leaf.append(root.left_child)
         if root.right_child.n_sample > 1:
@@ -25,27 +27,39 @@ class regression_tree:
             # Select a leaf to split among all leaves
             mse_min_global = sys.maxint
             split_node_global = None
-            split_fea_global = None
+            split_fea_index_global = None
             split_val_global = None
             
             for leaf in self.lst_leaf:
-                mse_min, split_fea, split_val, left_child, right_child =  leaf.try_split_node(criteria = 'MSE')
+                mse_min, split_fea_index, split_val, left_child, right_child =  leaf.try_split_node(criteria = 'MSE')
                 if mse_min < mse_min_global:
                     mse_min_global = mse_min
                     split_node_global = leaf
-                    split_fea_global = split_fea
+                    split_fea_index_global = split_fea_index
                     split_val_global = split_val
 
-            split_node_global.split_node(split_fea_global, split_val_global)
+            split_node_global.split_node(split_fea_index_global, split_val_global)
             if split_node_global.left_child.n_sample > 1:
                 self.lst_leaf.append(split_node_global.left_child)
             if split_node_global.right_child.n_sample > 1:
                 self.lst_leaf.append(split_node_global.right_child)
             current_n_node += 1
-        return root
+        self.root = root
 
-    
+    def predict_one_sample(self, sample):
+        # Go through the tree until the leaf node.
+        current_node = self.root
+        while not current_node.is_leaf:
+            split_fea_index = current_node.split_fea_index
+            split_fea_val = current_node.split_fea_val
+            if sample[split_fea_index] <= split_fea_val:
+                current_node = current_node.left_child
+            else:
+                current_node = current_node.right_child
+        return current_node.predict_val
 
-
-
-
+    def predict(self, lst_sample):
+        lst_predict_val = []
+        for sample in lst_sample:
+            lst_predict_val.append(self.predict_one_sample(sample))
+        return lst_predict_val
--- HDML/tree_based_model/tree_node.py
+++ HDML/tree_based_model/tree_node.py
@@ -2,61 +2,78 @@ import sys
 from HDML.util.functions import mean
 
 class tree_node:
-    def __init__(self, lst_sample):
+    def __init__(self, lst_sample, lst_feature):
+        self.lst_feature = lst_feature
         self.lst_sample = lst_sample
         self.n_sample = len(self.lst_sample)
         self.left_child = None
         self.right_child = None
         self.split_fea = None
-        self.split_val = 0
+        self.split_fea_val = 0
+        self.split_fea_index = 0
+        self.split_fea_index_tmp = 0
+        self.is_leaf = True
+        self.predict_val = mean([sample[-1] for sample in self.lst_sample])
         '''
         self.left_child_tmp = None
         self.right_child_tmp = None
         self.split_fea_tmp = None
-        self.split_val_tmp = 0
+        self.split_fea_val_tmp = 0
         '''
     def try_split_node(self, criteria = 'MSE'):
-        if not lst_sample:
+        if  len(self.lst_sample) <= 1:
             raise ValueError("Empty lst_sample! Can NOT split any more!")
         if criteria == "MSE":
             mse_min = sys.maxint
-            self.split_fea = -1
-            self.split_val = 0
+            self.split_fea_index = -1
+            self.split_fea_val = 0
             self.left_child = None
             self.right_child = None
-            for i_fea in len(self.lst_sample[0] - 1):
+            for index_fea in range(len(self.lst_sample[0]) - 1):
+                lst_flt_split = [sample[index_fea] for sample in self.lst_sample]
+                max_flt_split = max(lst_flt_split)
+                min_flt_split = min(lst_flt_split)
+                set_flt_split = set()
                 for sample_split in self.lst_sample:
                     lst_sample_left = []
                     lst_sample_right = []
-                    flt_split = sample_split[i_fea]
+                    flt_split = sample_split[index_fea]
+                    # If the split feature value is the max or min or has been evaluated before, then skip
+                    if flt_split == max_flt_split or flt_split == min_flt_split or flt_split in set_flt_split:
+                        continue
+                    set_flt_split.add(flt_split)
                     for sample in self.lst_sample:
-                        if sample[i_fea] <= flt_split:
+                        if sample[index_fea] <= flt_split:
                             lst_sample_left.append(sample)
                         else:
                             lst_sample_right.append(sample)
                     mean_left = mean([sample[-1] for sample in lst_sample_left])
                     mean_right = mean([sample[-1] for sample in lst_sample_right])
-                    mse = sum([(sample[-1] - mean_left) ** 2 for sample in mean_left]) + sum([(sample[-1] - mean_right) ** 2 for sample in mean_right])
+                    mse = sum([(sample[-1] - mean_left) ** 2 for sample in lst_sample_left]) + sum([(sample[-1] - mean_right) ** 2 for sample in lst_sample_right])
                     if mse < mse_min:
                         mse_min = mse
-                        self.split_fea_tmp = i_fea
-                        self.split_val_tmp = sample_split[i_fea]
-                        self.left_child_tmp = tree_node(lst_sample_left)
-                        self.right_child_tmp = tree_node(lst_sample_right)
-            return (mse_min, self.split_fea_tmp, self.split_val_tmp, self.left_child_tmp, self.right_child_tmp)
+                        self.split_fea_index_tmp = index_fea
+                        self.split_fea_val_tmp = sample_split[index_fea]
+                        self.left_child_tmp = tree_node(lst_sample_left, self.lst_feature)
+                        self.right_child_tmp = tree_node(lst_sample_right, self.lst_feature)
+            return (mse_min, self.split_fea_index_tmp, self.split_fea_val_tmp, self.left_child_tmp, self.right_child_tmp)
         elif criteria == "IG":
             pass
 
-    def split_node(self, split_fea, split_val):
+    def split_node(self, split_fea_index, split_fea_val):
+        self.is_leaf = False
         lst_sample_left = []
         lst_sample_right = []
         for sample in self.lst_sample:
-            if sample[split_fea] < = split_val:
+            if sample[split_fea_index] <= split_fea_val:
                 lst_sample_left.append(sample)
             else:
                 lst_sample_right.append(sample)
-        self.left_child = tree_node(lst_sample_left)
-        self.right_child = tree_node(lst_sample_right)
+        self.left_child = tree_node(lst_sample_left, self.lst_feature)
+        self.right_child = tree_node(lst_sample_right, self.lst_feature)
+        self.split_fea = self.lst_feature[split_fea_index]
+        self.split_fea_index = split_fea_index
+        self.split_fea_val = split_fea_val
 
             
 
